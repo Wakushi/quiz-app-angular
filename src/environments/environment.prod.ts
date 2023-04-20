ad753432fc05914d47d9e5fffd1b5bcdae949ff7
Wakushi <mzozord@gmail.com>
2023-04-21 00:22:47 +0200

    Vercel prod fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "FAKE_API_KEY",
+        authDomain: "fake-auth-domain.firebaseapp.com",
+        projectId: "fake-project-id",
+        storageBucket: "fake-storage-bucket.appspot.com",
+        messagingSenderId: "000000000000",
+        appId: "0:000000000000:web:0000000000000000",
+    },
+}
